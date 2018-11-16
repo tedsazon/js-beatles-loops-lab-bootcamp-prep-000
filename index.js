3f88052e30a999524724eb3e72d6680afe83d021
Ted Sazon <tedsazon@gmail.com>
2018-11-16 21:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var allMusicians = []
+  for ( var i = 0; musicians.length; i++) {
+    allMusicians.push(musicians[i] + "plays" + instruments[i])
+  }
+  return allMusicians
+}
