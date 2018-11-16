fe0b7d5ff91483289459d3190154fd25299261b1
Ted Sazon <tedsazon@gmail.com>
2018-11-16 21:40:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function johnLennonFacts() {
   var facts = [
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
-    "He was a choir boy and a boy scout",
+    "He was a choir boy and boy scout",
     "He hated the sound of his own voice"
   ]
   var newFacts = []
